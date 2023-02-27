757cc9a3564a2dd3ff1574c759db8752531cf51b
KWink1 <KWink1@N-FI-01-3756>
2023-02-27 16:02:26 +0200

    Feat: Models classes created

--- MovieCharactersAPI/Models/Movie.cs
+++ MovieCharactersAPI/Models/Movie.cs
@@ -1,6 +1,30 @@
-﻿namespace MovieCharactersAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCharactersAPI.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Genre { get; set; }
+        [Required]
+        public int ReleaseYear { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Director { get; set; }
+        [Required]
+        [Url]
+        public string Picture { get; set; }
+        [Required]
+        [Url]
+        public string Trailer { get; set; }
+        [Required]
+        public ICollection<Character> Characters { get; set; }
+        [Required]
+        public Franchise FranchiseId { get; set; }
     }
 }
