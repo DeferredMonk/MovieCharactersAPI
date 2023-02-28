756fa3b026b0a7a44390bfba320e7ccdb80675f4
KWink1 <KWink1@N-FI-01-3756>
2023-02-28 14:03:02 +0200

    Fix: Recreated database

--- MovieCharactersAPI/Models/Character.cs
+++ MovieCharactersAPI/Models/Character.cs
@@ -5,17 +5,14 @@ namespace MovieCharactersAPI.Models
     public class Character
     {
         public int Id { get; set; }
-        [Required]
         [MaxLength(30)]
         public string Name { get; set; }
         [MaxLength(30)]
-        public string Alias { get; set; }
-        [Required]
+        public string? Alias { get; set; }
         [MaxLength(15)]
         public string Gender { get; set; }
-        [Required]
         [Url]
-        public string Picture { get; set;}
+        public string Picture { get; set; }
         public ICollection<Movie> Movies { get; set; }
     }
 }
--- MovieCharactersAPI/Models/Franchise.cs
+++ MovieCharactersAPI/Models/Franchise.cs
@@ -5,10 +5,8 @@ namespace MovieCharactersAPI.Models
     public class Franchise
     {
         public int Id { get; set; }
-        [Required]
         [MaxLength(30)]
         public string Name { get; set; }
-        [Required]
         [MaxLength(150)]
         public string Description { get; set; }
         public ICollection<Movie> Movies { get; set; }
